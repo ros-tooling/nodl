1605f3f73184e3907d101336dc5bf798dc8ce484
Alistair English <hello@alistairenglish.com>
2026-08-12 17:19:51 +1000

    base package scaffolding
    Signed-off-by: Alistair English <hello@alistairenglish.com>

---
+++ nodl_generator_cpp/nodl_generator_cpp/__main__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 Open Source Robotics Foundation, Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""Entry point for ``python -m nodl_generator_cpp``."""
+
+import sys
+
+
+def main() -> int:
+    print('nodl_generator_cpp: not yet implemented')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
